96c30a82d965cc691661c9e1771c86e367e721c0
Malte Tancred <malte@tancred.com>
2014-02-12 23:07:40 +0100

    Cosmetics.

--- tweetnacl-objc.h
+++ tweetnacl-objc.h
@@ -1,13 +1,5 @@
 #import <Foundation/Foundation.h>
 
-NSData *ObjcNaClBoxKeypair(NSData **sk, NSError **error);
-
-NSData *ObjcNaClBox(NSData *m, NSData *n, NSData *pk, NSData *sk, NSError **error);
-NSData *ObjcNaClBoxOpen(NSData *c, NSData *n, NSData *pk, NSData *sk, NSError **error);
-
-BOOL ObjcNaClBoxBeforeNM(NSData **k, NSData *pk, NSData *sk, NSError **error);
-NSData *ObjcNaClBoxAfterNM(NSData *m, NSData *n, NSData *k, NSError **error);
-
 @interface CryptoBoxKey : NSObject
 @property(copy,readonly,nonatomic) NSData *keyData;
 + (instancetype)keyWithData:(NSData *)data error:(NSError **)error;
@@ -34,31 +26,13 @@ NSData *ObjcNaClBoxAfterNM(NSData *m, NSData *n, NSData *k, NSError **error);
 
 extern NSString * const ObjcNaClErrorDomain;
 
-/*
-@interface ObjcNaClBox : NSObject
-@property(copy) NSData *k;
-- (id)initWithPublicKey:(NSData *)pk secretKey:(NSData *)sk error:(NSError *)error;
-- (BOOL)precomputeWithPublicKey:(NSData *)pk secrectKey:(NSData *)sk error:(NSError *)error;
-- (NSData *)boxMessage:(NSData *)m nonce:(NSData *)n error:(NSError **)error;
-- (NSData *)openMessage:(NSData *)m nonce:(NSData *)n error:(NSError **)error;
-@end
+// deprecated
 
-// or without NSError?
+NSData *ObjcNaClBoxKeypair(NSData **sk, NSError **error);
 
-NSData *ObjcNaClBoxKeypair(NSData **sk);
+NSData *ObjcNaClBox(NSData *m, NSData *n, NSData *pk, NSData *sk, NSError **error);
+NSData *ObjcNaClBoxOpen(NSData *c, NSData *n, NSData *pk, NSData *sk, NSError **error);
 
-NSData *ObjcNaClBox(NSData *m, NSData *n, NSData *pk, NSData *sk);
-NSData *ObjcNaClBoxOpen(NSData *c, NSData *n, NSData *pk, NSData *sk);
+BOOL ObjcNaClBoxBeforeNM(NSData **k, NSData *pk, NSData *sk, NSError **error);
+NSData *ObjcNaClBoxAfterNM(NSData *m, NSData *n, NSData *k, NSError **error);
 
-BOOL ObjcNaClBoxBeforeNM(NSData **k, NSData *pk, NSData *sk);
-NSData *ObjcNaClBoxAfterNM(NSData *m, NSData *n, NSData *k);
-
-@interface ObjcNaClBox : NSObject
-@property(copy) NSData *k;
-- (id)initWithPublicKey:(NSData *)pk secretKey:(NSData *)sk;
-- (BOOL)precomputeWithPublicKey:(NSData *)pk secrectKey:(NSData *)sk;
-- (NSData *)boxMessage:(NSData *)m nonce:(NSData *)n;
-- (NSData *)openMessage:(NSData *)m nonce:(NSData *)n;
-@end
-
-*/
